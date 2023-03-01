7e88c5b868e8be394e18ddfb4f02742dcf0014b1
emintatar <emintatar00@gmail.com>
2023-03-01 20:24:49 +0300

    first commit

---
+++ PurchasingSystem/Phone.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PurchasingSystem
+{
+    class Phone
+    {
+        public string firstName;
+        public string lastName;
+        public string phoneBrand;
+        public string operatingSystem;
+        public int ramSize;
+        public int phonePrice;
+        public int balance;
+
+        public Phone(string firstName, string lastName, string phoneBrand, string operatingSystem, int ramSize, int phonePrice)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.phoneBrand = phoneBrand;
+            this.operatingSystem = operatingSystem;
+            this.ramSize = ramSize;
+            this.phonePrice = phonePrice;
+        }
+
+        public void showPersonalInformations()
+        {
+            Console.WriteLine("First Name: " + firstName);
+            Console.WriteLine("Last Name: " + lastName);
+            Console.WriteLine("Balance: " + balance);
+        }
+
+        public void showPhoneInformations()
+        {
+            Console.WriteLine("Operating System: " + operatingSystem);
+            Console.WriteLine("Phone Brand: " + phoneBrand);
+            Console.WriteLine("Ram Size: " + ramSize);
+            Console.WriteLine("Phone Price: " + phonePrice);
+        }
+
+        public void checkAffordability()
+        {
+            if (balance >= phonePrice)
+            {
+                Console.WriteLine("You have enough money.");
+                Console.WriteLine("You can buy this phone!");
+            }
+
+            else
+            {
+                Console.WriteLine("You do not have enough money.");
+                Console.WriteLine("You can not buy this phone!");
+            }
+        }
+    }
+}
---
+++ PurchasingSystem/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace PurchasingSystem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            void printOptions()
+            {
+                Console.WriteLine("PLEASE CHOOSE FROM ONE OF THE FOLLOWING OPTIONS");
+                Console.WriteLine("1. Personel Informations");
+                Console.WriteLine("2. Phone Informations");
+                Console.WriteLine("3. Check Affordability");
+                Console.WriteLine("4. Exit");
+            }
+
+            Phone phoneInstance = new Phone("Emin", "Tatar", "Huawei", "Android", 8, 5000);
+
+            int option = 0;
+
+            while (option != 4)
+            {
+                printOptions();
+
+                try
+                {
+                    option = int.Parse(Console.ReadLine());
+                    Console.Clear();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error:" + e);
+                }
+
+                if (option == 1)
+                {
+                    phoneInstance.showPersonalInformations();
+                }
+
+                else if (option == 2)
+                {
+                    phoneInstance.showPhoneInformations();
+                }
+
+                else if (option == 3)
+                {
+                    Console.Write("Enter your current balance: ");
+                    phoneInstance.balance = int.Parse(Console.ReadLine());
+                    phoneInstance.checkAffordability();
+                }
+
+                else if (option == 4)
+                {
+                    break;
+                }
+
+                else
+                {
+                    option = 0;
+                }
+
+            }
+
+            Console.WriteLine("Thank You!");
+            Console.WriteLine("Have a Nice Day :)");
+        }
+    }
+}
